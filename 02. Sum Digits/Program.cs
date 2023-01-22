79d9736abff04bb3f713a98fd0c481d9e53b2f2a
cr0w- <cr0w-@MILEN-PC>
2023-01-22 14:48:58 +0200

    sync

---
+++ 02. Sum Digits/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = int.Parse(Console.ReadLine());
+int tempVar;
+int sum = 0;
+
+while (num > 0)
+{
+    tempVar = num % 10;
+    sum += tempVar;
+    num /= 10;
+}
+
+Console.WriteLine(sum); 
