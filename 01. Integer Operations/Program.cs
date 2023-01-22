fa7dab79572fb95fb6d1249564715649048fefa6
cr0w- <cr0w-@MILEN-PC>
2023-01-22 14:39:38 +0200

    sync

---
+++ 01. Integer Operations/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+int numThree = int.Parse(Console.ReadLine());
+int numFour = int.Parse(Console.ReadLine());
+
+int total = 0;
+
+total = numOne + numTwo;
+total /= numThree;
+total *= numFour;
+
+Console.WriteLine(total);
