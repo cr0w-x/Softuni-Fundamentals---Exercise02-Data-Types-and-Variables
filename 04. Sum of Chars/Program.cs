2379b6b1c1095287e5c73471793aa02d08a12dad
cr0w- <cr0w-@MILEN-PC>
2023-01-23 11:08:34 +0200

    sync

---
+++ 04. Sum of Chars/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberOfLines = int.Parse(Console.ReadLine());
+char[] symbolArray = new char[numberOfLines];
+
+for (int i = 0; i < symbolArray.Length; i++)
+{
+    symbolArray[i] = char.Parse(Console.ReadLine());
+}
+
+int totalSum = 0;
+
+for (int i = 0; i < symbolArray.Length; i++)
+{
+    totalSum += (int)symbolArray[i];
+}
+
+Console.WriteLine($"The sum equals: {totalSum}");
