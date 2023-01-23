bb87358e6e6747a8d5f9f6743b0e825e5df4baf7
cr0w- <cr0w-@MILEN-PC>
2023-01-23 10:49:13 +0200

    sync

---
+++ 03. Elevator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+int courses = n / p;
+
+if (n % p != 0)
+    courses++;
+
+Console.WriteLine(courses);
