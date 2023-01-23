a759ff425520adc891282d28f084c1fa97a137fb
cr0w- <cr0w-@MILEN-PC>
2023-01-23 12:03:41 +0200

    sync

---
+++ 06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+
+foreach (char firstNum in Enumerable.Range('a', (char)n))
+{
+    foreach (char secondNum in Enumerable.Range('a', (char)n))
+    {
+        foreach (char thirdNum in Enumerable.Range('a', (char)n))
+        {
+            Console.Write(firstNum);
+            Console.Write(secondNum);
+            Console.Write(thirdNum);
+            Console.WriteLine();
+        }
+
+    }
+
+}
