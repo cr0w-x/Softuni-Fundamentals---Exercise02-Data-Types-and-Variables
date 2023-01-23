bdd28591c4b287f7be6bba2e48adb0108b2aacf4
cr0w- <cr0w-@MILEN-PC>
2023-01-23 13:15:29 +0200

    sync

---
+++ 08. Beer Kegs/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+double volumeOfBiggestKeg = 0;
+string nameOfBiggestKeg = string.Empty;
+
+for (int i = 0; i < n; i++)
+{
+    string inputName = Console.ReadLine();
+    float inputRadius = float.Parse(Console.ReadLine());
+    int inputHeight = int.Parse(Console.ReadLine());
+
+    double tempVar = Math.PI * (inputRadius * inputRadius) * inputHeight;
+
+    if (tempVar >= volumeOfBiggestKeg)
+    {
+        volumeOfBiggestKeg = tempVar;
+        nameOfBiggestKeg = inputName;
+    }
+
+}
+
+Console.WriteLine(nameOfBiggestKeg);
