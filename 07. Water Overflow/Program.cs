208c35c3bb399fb55053e379a2975490c99b7a36
cr0w- <cr0w-@MILEN-PC>
2023-01-23 12:30:41 +0200

    sync

---
+++ 07. Water Overflow/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int totalWater = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int quantitiesOfWater = int.Parse(Console.ReadLine());
+
+    if (totalWater + quantitiesOfWater > 255)
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+
+    else
+    {
+        totalWater+= quantitiesOfWater;
+    }
+
+}
+
+Console.WriteLine(totalWater);
